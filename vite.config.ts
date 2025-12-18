1a79226179e83c6dfc035d0357087b49a83b180c
yuhhueysu-debug <yuhhueysu@gmail.com>
2025-12-18 16:21:01 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,12 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  plugins: [react()],
+  // 這裡的 base 必須對應您的 GitHub 專案名稱
+  base: '/TCVGH-library_Game/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 });
